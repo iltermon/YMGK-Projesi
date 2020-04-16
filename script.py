d20d9652f1027430f9cd9c86b668bd76a889f1a9
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-16 23:45:33 +0300

    2019  yılı tüm ayların ortalaması alındı.

---
+++ script.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+def fillEmptyViaMean(df):
+    
+    temp = df
+df=pd.read_csv("veri_seti/veri_seti_aylik.csv", sep=';', encoding = 'utf8')
+print)
+
+
