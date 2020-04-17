bd3b137008b31e21463395062856df0acb1a8855
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-17 20:12:52 +0300

    indeksleme için bir index sütunu eklendi.

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-def fillEmptyViaMean(df):
-    
-    temp = df
-df=pd.read_csv("veri_seti/veri_seti_aylik.csv", sep=';', encoding = 'utf8')
-print)
+df=pd.read_csv("veri_seti/ay_ortalama.csv", sep=';', encoding = 'utf8')
+plt.bar(df["Tarih"],df["PM10"])
+plt.show()
 
 
