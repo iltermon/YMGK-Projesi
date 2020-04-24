d516c2ffba853a0d36ef284ac349d82f70f915ff
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-24 23:38:25 +0300

    veri seti özeti oluşturuldu.

---
+++ metrics.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("veri_seti\\veri_seti(temizlenmis).csv",sep=";")
+print("max")
+print(df.max(axis=0))
+print("min")
+print(df.min(axis=0))
+print("ort")
+print(df.mean(axis=0))
+print("std")
+print(df.std(axis=0))
